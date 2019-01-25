c68de57ac5e0e9a8450cec718b117e5843625035
EmaSuriano <emanuel.suriano@gmail.com>
2019-01-25 15:25:36 -0300

    First commit

---
+++ gatsby-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby My Followers`,
+    description: `A starter to show your followers on different platforms`,
+    author: `@emasuriano`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-my-followers`,
+        short_name: `My Followers`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.app/offline
+    'gatsby-plugin-offline',
+  ],
+}
